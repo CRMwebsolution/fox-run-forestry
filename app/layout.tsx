d776e92a81697858c66dec0a50150c22767d47ee
CRMwebsolution <cody@southernautomate.com>
2026-07-31 07:03:19 -0400

    Update browser theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#17372d",
+  themeColor: "#0b0c09",
 };
 
 const localBusinessJsonLd = {
